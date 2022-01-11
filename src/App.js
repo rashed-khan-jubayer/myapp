dde4684cf60dd400c403fd763a69862c5d9aaedb
rashed-khan-jubayer <rashedkhanjubayer018@gmail.com>
2022-01-11 13:31:11 +0600

    sec com

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const adobe = ['Adobe PDF','Photoshop','Illustrtor']
   return (
     <div className="App">
       <header className="App-header">
         <h1>I am exploring React</h1>
         <Person name="Parrot"></Person>
+        <ul>
+          {adobe.map(ad => <li>{ad}</li>)}
+        </ul>
       </header>
     </div>
     
@@ -15,14 +19,14 @@ function App() {
 
 function Person(prob){
   const personStyle = {
-    border:'2px solid red',
+    border:'2px solid green',
     margin: '10px',
     padding: '2px'
   }
   return(
     <div style={personStyle}>
       <h1>Name: {prob.name}</h1>
-      <p></p>
+      <p>Lorem ipsum dolor sit amet.</p>
     </div>
   );
 }
